15c0b81a6e11306ee4691e561aeb4c678d42a8ba
guabu <135956181+guabu@users.noreply.github.com>
2025-01-07 11:05:34 +0100

    chore: footer theming (#3)

--- apps/dashboard/app/dashboard/layout.tsx
+++ apps/dashboard/app/dashboard/layout.tsx
@@ -5,7 +5,6 @@ import { BellIcon, SlashIcon } from "lucide-react"
 
 import { appClient, managementClient } from "@/lib/auth0"
 import { Button } from "@/components/ui/button"
-import { Auth0Logo } from "@/components/auth0-logo"
 import Header from "@/components/Header"
 import { ModeToggle } from "@/components/mode-toggle"
 import { OrganizationSwitcher } from "@/components/organization-switcher"
@@ -37,7 +36,6 @@ export default async function DashboardLayout({
   return (
     <UserProvider>
       <div>
-        {/* Top bar: Organization Switcher and Profile in Layout.tsx */}
         <div className="mx-auto flex max-w-full items-center justify-between px-2 pb-2 pt-4 sm:px-6">
           <div className="flex items-center space-x-2">
             {/* Vercel Logo  */}
@@ -103,42 +101,80 @@ export default async function DashboardLayout({
           </div>
         </div>
 
-        {/* Horizontal Menu from Vercel Dashboard's Header.tsx */}
         <Header />
 
-        {/* This handles the navigation menu (Overview, Integrations, etc.) */}
-        {/* Main element here scopes page.tsx elements */}
         <main className="mx-auto grid min-h-[calc(100svh-164px)] max-w-full bg-secondary pb-12 dark:bg-inherit">
           {children}
         </main>
 
-        <footer className="mx-auto max-w-7xl px-2 py-6 sm:px-6 lg:px-8">
-          <div className="flex justify-between">
-            <div className="flex items-center space-x-2">
-              <Auth0Logo className="h-6 w-6" />
-
-              <div className="font-mono font-semibold">
-                <Link href="/">SaaStart</Link>
+        <footer className="mx-auto border-t p-6">
+          <div className="container mx-auto max-w-7xl py-4">
+            <div className="flex flex-col justify-between gap-y-4 sm:flex-row sm:items-center">
+              <div className="flex flex-col gap-y-4 sm:flex-row sm:items-center sm:space-x-6">
+                <Link href="/">
+                  <VercelSvg className="size-5" />
+                </Link>
+                <nav className="grid grid-cols-2 gap-6 text-sm sm:grid-cols-6">
+                  <Link
+                    href="https://vercel.com/"
+                    className="text-muted-foreground transition-colors hover:text-primary"
+                  >
+                    Home
+                  </Link>
+                  <Link
+                    href="https://vercel.com/docs"
+                    className="text-muted-foreground transition-colors hover:text-primary"
+                  >
+                    Docs
+                  </Link>
+                  <Link
+                    href="https://vercel.com/guides"
+                    className="text-muted-foreground transition-colors hover:text-primary"
+                  >
+                    Guides
+                  </Link>
+                  <Link
+                    href="https://vercel.com/help"
+                    className="text-muted-foreground transition-colors hover:text-primary"
+                  >
+                    Help
+                  </Link>
+                  <Link
+                    href="https://vercel.com/contact"
+                    className="text-muted-foreground transition-colors hover:text-primary"
+                  >
+                    Contact
+                  </Link>
+                  <Link
+                    href="https://vercel.com/legal"
+                    className="text-muted-foreground transition-colors hover:text-primary"
+                  >
+                    Legal
+                  </Link>
+                </nav>
               </div>
 
-              <div>
-                <Button variant="link" asChild>
-                  <Link href="/">Home</Link>
-                </Button>
-
-                <Button variant="link" asChild>
-                  <Link
-                    href="https://github.com/auth0-developer-hub/auth0-b2b-saas-starter"
-                    target="_blank"
-                  >
-                    Source
+              <div className="-ml-3 flex items-center justify-between">
+                <Button
+                  className="flex items-center"
+                  variant="ghost"
+                  size="sm"
+                  asChild
+                >
+                  <Link href="https://vercel-status.com">
+                    <div className="mr-2 h-2 w-2 rounded-full bg-blue-500"></div>
+                    <span className="text-sm text-blue-500">
+                      All systems normal
+                    </span>
                   </Link>
                 </Button>
+
+                <ModeToggle />
               </div>
             </div>
 
-            <div className="items-center gap-x-2">
-              <ModeToggle />
+            <div className="mt-4 text-xs text-muted-foreground">
+              © {new Date().getFullYear()}, Vercel Inc.
             </div>
           </div>
         </footer>
