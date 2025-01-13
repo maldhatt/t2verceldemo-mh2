824ae9b0612057f67d25dd8a66c61a56434eb20e
guabu <135956181+guabu@users.noreply.github.com>
2025-01-13 09:16:48 +0100

    feat: poll and redirect (#7)

---
+++ apps/dashboard/app/dashboard/poll-and-redirect.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import { useEffect } from "react"
+import { useRouter } from "next/navigation"
+import useSWR from "swr"
+
+const fetcher = (...args: [string, RequestInit?]): Promise<any> =>
+  fetch(...args).then((res) => {
+    // if status code is 204, it means the user is not authenticated
+    if (res.status === 204) {
+      return null
+    }
+
+    if (!res.ok) {
+      return null
+    }
+
+    return res.json()
+  })
+
+export function PollAndRedirect() {
+  const { data, error, isLoading } = useSWR("/api/auth/me", fetcher, {
+    refreshInterval: 5000, // poll every 5 seconds
+  })
+  const router = useRouter()
+
+  useEffect(() => {
+    if (error) return
+    if (isLoading) return
+
+    if (!data) {
+      void router.push("/")
+    }
+  }, [data, router])
+
+  return null
+}
